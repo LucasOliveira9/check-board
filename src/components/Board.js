a4a0513d7fa53c40fb10e37a4efa91bde17e4226
Lucas Oliveira Rodrigues <lucasolliveiraa1@gmail.com>
2025-10-07 19:34:41 -0300

    Initial Commit: setup project with Vite + React

---
+++ src/components/Board.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+function Board() {
+    return (_jsx("div", { children: _jsx("h1", { children: "DEMO" }) }));
+}
+export default Board;
---
+++ src/components/Board.tsx
@@ -0,0 +1,9 @@
+function Board() {
+  return (
+    <div>
+      <h1>DEMO</h1>
+    </div>
+  );
+}
+
+export default Board;
