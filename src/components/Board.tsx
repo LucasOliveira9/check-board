824634b3cdae9cfca41e5337119011b5fde9c7f1
Lucas Oliveira Rodrigues <lucasolliveiraa1@gmail.com>
2025-10-07 21:11:48 -0300

    efactor: clean up Board.tsx and index.ts exports

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,9 +1,38 @@
-function Board() {
-  return (
-    <div>
-      <h1>DEMO</h1>
-    </div>
-  );
-}
+import { useEffect, useRef } from "react";
+import Draw from "../draw/drawBoard";
+import { TBoard } from "../types/board";
+
+const Board: React.FC<TBoard> = ({
+  size,
+  isBlackView,
+  lightTile,
+  blackTile,
+}) => {
+  const drawRef = useRef<number>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext("2d");
+    if (!canvas || !ctx) return;
+    // manter qualidade
+    const dpr = window.devicePixelRatio || 1;
+    canvas.width = size * dpr;
+    canvas.height = size * dpr;
+    canvas.style.width = size + "px";
+    canvas.style.height = size + "px";
+    ctx.scale(dpr, dpr);
+
+    const render = (time: number) => {
+      Draw({ size, isBlackView, canvas, lightTile, blackTile });
+      drawRef.current = requestAnimationFrame(render);
+    };
+    drawRef.current = requestAnimationFrame(render);
+
+    return () => {
+      if (drawRef.current) cancelAnimationFrame(drawRef.current);
+    };
+  }, [size, isBlackView]);
+  return <canvas ref={canvasRef} />;
+};
 
 export default Board;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { default as Board } from "./components/Board";
+export { default as Draw } from "./draw/drawBoard";
+export type { TBoard } from "./types/board";
+export type { TDrawBoard } from "./types/draw";
