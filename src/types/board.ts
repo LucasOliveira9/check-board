68b82f92492010a41a2c21e46a975d951ac703a7
Lucas Oliveira Rodrigues <lucasolliveiraa1@gmail.com>
2025-10-08 14:24:55 -0300

    feat(types): add config object and TConfig type to TBoard type

--- src/types/board.ts
+++ src/types/board.ts
@@ -1,6 +1,10 @@
 export type TBoard = {
-  size: number;
+  config: TConfig;
+};
+
+export type TConfig = {
   isBlackView: boolean;
+  size: number;
   lightTile: string;
-  blackTile: string;
+  darkTile: string;
 };
