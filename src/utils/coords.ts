453863a87563139e67cadc3706b8010d3503a49f
Lucas Oliveira Rodrigues <lucasolliveiraa1@gmail.com>
2025-10-07 21:24:45 -0300

    feat: add utils

---
+++ src/utils/coords.ts
@@ -0,0 +1,45 @@
+import { TFile, TRank, TSquare } from "../types/square";
+
+const files = "abcdefgh";
+const squareToCoords = (
+  square: TSquare | null,
+  squareSize: number,
+  isBlackView: boolean
+) => {
+  if (square) {
+    const file = files.indexOf(square.file);
+    const rank = square.rank - 1;
+    if (!isBlackView) {
+      return { x: file * squareSize, y: (7 - rank) * squareSize };
+    }
+    return { x: (7 - file) * squareSize, y: rank * squareSize };
+  }
+};
+
+const coordsToSquare = (
+  x: number,
+  y: number,
+  squareSize: number,
+  isBlackView: boolean
+): { file: TFile; rank: TRank } | null => {
+  let file = Math.floor(x / squareSize);
+  let rank = 7 - Math.floor(y / squareSize);
+  if (file < 0 || file > 7 || rank < 0 || rank > 8) return null;
+  if (isBlackView) {
+    file = 7 - Math.floor(x / squareSize);
+    rank = Math.floor(y / squareSize);
+  }
+  const file_ = files[file] as TFile, //toFile(files[file]),
+    rank_ = (rank + 1) as TRank; //toRank(rank + 1);
+
+  if (file_ && rank_) return { file: file_, rank: rank_ };
+  return null;
+};
+
+const getCanvasCoords = (e: React.PointerEvent<HTMLCanvasElement>) => {
+  const rect = e.currentTarget.getBoundingClientRect();
+
+  return { offsetX: e.clientX - rect.left, offsetY: e.clientY - rect.top };
+};
+
+export { getCanvasCoords, coordsToSquare, files, squareToCoords };
